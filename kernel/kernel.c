2ce4bba3f747e2ceee6100c57c25eba22527ee25
emm312 <emmtbm55@gmail.com>
2022-02-27 22:56:02 +1100

    .

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,11 @@
-/* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
-void dummy_test_entrypoint() {
-}
+#include "../drivers/screen.h"
+#include "util.h"
+#include "../cpu/isr.h"
+#include "../cpu/idt.h"
 
 void main() {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    isr_install();
+    /* Test the interrupts */
+    __asm__ __volatile__("int $2");
+    __asm__ __volatile__("int $3");
 }
