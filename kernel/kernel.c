88a624c22ed23067033ee16919f82d2d46237732
emm312 <emmtbm55@gmail.com>
2022-02-28 19:54:55 +1100

    .

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -33,7 +33,8 @@ void user_input(char *input) {
         kprint(", physical address: ");
         kprint(phys_str);
         kprint("\n");
-    }
+        
+    } 
     kprint("You said: ");
     kprint(input);
     kprint("\n> ");
